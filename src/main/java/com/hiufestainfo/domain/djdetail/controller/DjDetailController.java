be55dcc0d7cdff3de319e57737d9a167d5358f3e
JSHWJ <bogum1029@gmail.com>
2023-09-06 20:02:58 +0900

    feat:DjDetail 에러코드 구현

--- src/main/java/com/hiufestainfo/domain/djdetail/controller/DjDetailController.java
+++ src/main/java/com/hiufestainfo/domain/djdetail/controller/DjDetailController.java
@@ -2,6 +2,7 @@ package com.hiufestainfo.domain.djdetail.controller;
 
 import com.hiufestainfo.domain.djdetail.dto.DjDetailDto;
 import com.hiufestainfo.domain.djdetail.service.DjDetailService;
+import com.hiufestainfo.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,35 +17,34 @@ public class DjDetailController {
     private final DjDetailService djDetailService;
 
     @PostMapping
-    public ResponseEntity<String> createDjDetail(@RequestBody DjDetailDto djDetailDto) {
+    public ResponseEntity<SuccessResponse<String>> createDjDetail(@RequestBody DjDetailDto djDetailDto) {
         djDetailService.createDjDetail(djDetailDto);
         String message = "Successfully created";
-        return ResponseEntity.status(HttpStatus.CREATED).body(message);
+        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.of(message));
     }
 
     @GetMapping
-    public ResponseEntity<List<DjDetailDto>> getAllDjDetails() {
+    public ResponseEntity<SuccessResponse<List<DjDetailDto>>> getAllDjDetail() {
         List<DjDetailDto> djDetails = djDetailService.getAllDjDetails();
-        return ResponseEntity.ok(djDetails);
+        return ResponseEntity.ok(SuccessResponse.of(djDetails));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DjDetailDto> getDjDetail(@PathVariable Long id) {
+    public ResponseEntity<SuccessResponse<DjDetailDto>> getDjDetail(@PathVariable Long id) {
         DjDetailDto djDetail = djDetailService.getDjDetail(id);
-        return ResponseEntity.ok(djDetail);
+        return ResponseEntity.ok(SuccessResponse.of(djDetail));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updateDjDetail(@PathVariable Long id, @RequestBody DjDetailDto djDetailDto) {
+    public ResponseEntity<SuccessResponse<Void>> updateDjDetail(@PathVariable Long id, @RequestBody DjDetailDto djDetailDto) {
         djDetailService.updateDjDetail(id, djDetailDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(SuccessResponse.empty());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteDjDetail(@PathVariable Long id) {
+    public ResponseEntity<SuccessResponse<Void>> deleteDjDetail(@PathVariable Long id) {
         djDetailService.deleteDjDetail(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(SuccessResponse.empty());
     }
 }
-
 
