818482b0556102ef6988c263e0639fb0c5e841f0
JSHWJ <bogum1029@gmail.com>
2023-09-06 20:09:27 +0900

    fix:서비스 로직 수정

--- src/main/java/com/hiufestainfo/domain/djdetail/service/DjDetailService.java
+++ src/main/java/com/hiufestainfo/domain/djdetail/service/DjDetailService.java
@@ -3,8 +3,12 @@ package com.hiufestainfo.domain.djdetail.service;
 
 import com.hiufestainfo.domain.djdetail.dto.DjDetailDto;
 import com.hiufestainfo.domain.djdetail.entity.DjDetail;
+import com.hiufestainfo.domain.djdetail.exception.CreateDjDetailBadRequestException;
+import com.hiufestainfo.domain.djdetail.exception.DjDetailNotFoundException;
+import com.hiufestainfo.domain.djdetail.exception.DjDetailServiceException;
 import com.hiufestainfo.domain.djdetail.repository.DjDetailRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,39 +24,58 @@ public class DjDetailService {
     private final DjDetailRepository djDetailRepository;
 
     public void createDjDetail(DjDetailDto djDetailDto) {
-        DjDetail newFestDetail = djDetailDto.toEntity();
-        djDetailRepository.save(newFestDetail);
+        DjDetail newDjDetail = djDetailDto.toEntity();
+        try {
+            djDetailRepository.save(newDjDetail);
+        } catch (DataAccessException ex) {
+            throw new CreateDjDetailBadRequestException();
+        }
     }
 
     public DjDetailDto getDjDetail(Long id) {
         DjDetail djDetail = djDetailRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당하는 정보를 찾을 수 없습니다."));
+                .orElseThrow(() -> new DjDetailNotFoundException());
         return DjDetailDto.fromEntity(djDetail);
     }
 
     public List<DjDetailDto> getAllDjDetails() {
-        List<DjDetail> djDetails = djDetailRepository.findAll();
-        return djDetails.stream()
-                .map(DjDetailDto::fromEntity)
-                .collect(Collectors.toList());
+        try {
+            List<DjDetail> djDetails = djDetailRepository.findAll();
+            if (djDetails.isEmpty()) {
+                throw new DjDetailNotFoundException();
+            }
+            return djDetails.stream()
+                    .map(DjDetailDto::fromEntity)
+                    .collect(Collectors.toList());
+        } catch (DataAccessException ex) {
+            throw new DjDetailServiceException();
+        }
     }
 
     public void updateDjDetail(Long id, DjDetailDto updatedDjDetailDto) {
         DjDetail djDetailToUpdate = djDetailRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당하는 정보를 찾을 수 없습니다."));
+                .orElseThrow(() -> new DjDetailNotFoundException());
 
         djDetailToUpdate = updatedDjDetailDto.toEntity().toBuilder()
                 .id(id)
                 .build();
 
-        djDetailRepository.save(djDetailToUpdate);
+        try {
+            djDetailRepository.save(djDetailToUpdate);
+        } catch (DataAccessException ex) {
+            throw new DjDetailServiceException();
+        }
     }
 
     public void deleteDjDetail(Long id) {
         if (!djDetailRepository.existsById(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당하는 정보를 찾을 수 없습니다.");
+            throw new DjDetailNotFoundException();
         }
-        djDetailRepository.deleteById(id);
+        try {
+            djDetailRepository.deleteById(id);
+        } catch (DataAccessException ex) {
+            throw new DjDetailServiceException();
+        }
     }
 }
 
