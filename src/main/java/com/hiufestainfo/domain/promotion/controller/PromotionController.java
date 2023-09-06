52908f013bea79bbfce083cfd790248353e7226b
JSHWJ <bogum1029@gmail.com>
2023-09-06 19:33:09 +0900

    fix:promotion controller 코드 수정

--- src/main/java/com/hiufestainfo/domain/promotion/controller/PromotionController.java
+++ src/main/java/com/hiufestainfo/domain/promotion/controller/PromotionController.java
@@ -2,6 +2,7 @@ package com.hiufestainfo.domain.promotion.controller;
 
 import com.hiufestainfo.domain.promotion.dto.PromotionDto;
 import com.hiufestainfo.domain.promotion.service.PromotionService;
+import com.hiufestainfo.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,34 +17,33 @@ public class PromotionController {
     private final PromotionService promotionService;
 
     @PostMapping
-    public ResponseEntity<String> createPromotion(@RequestBody PromotionDto promotionDto) {
+    public ResponseEntity<SuccessResponse<String>> createPromotion(@RequestBody PromotionDto promotionDto) {
         promotionService.createPromotion(promotionDto);
         String message = "Successfully created";
-        return ResponseEntity.status(HttpStatus.CREATED).body(message);
+        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.of(message));
     }
 
     @GetMapping
-    public ResponseEntity<List<PromotionDto>> getAllPromotions() {
+    public ResponseEntity<SuccessResponse<List<PromotionDto>>> getAllPromotions() {
         List<PromotionDto> promotions = promotionService.getAllPromotions();
-        return ResponseEntity.ok(promotions);
+        return ResponseEntity.ok(SuccessResponse.of(promotions));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PromotionDto> getPromotion(@PathVariable Long id) {
+    public ResponseEntity<SuccessResponse<PromotionDto>> getPromotion(@PathVariable Long id) {
         PromotionDto promotion = promotionService.getPromotion(id);
-        return ResponseEntity.ok(promotion);
+        return ResponseEntity.ok(SuccessResponse.of(promotion));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updatePromotion(@PathVariable Long id, @RequestBody PromotionDto promotionDto) {
+    public ResponseEntity<SuccessResponse<Void>> updatePromotion(@PathVariable Long id, @RequestBody PromotionDto promotionDto) {
         promotionService.updatePromotion(id, promotionDto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(SuccessResponse.empty());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePromotion(@PathVariable Long id) {
+    public ResponseEntity<SuccessResponse<Void>> deletePromotion(@PathVariable Long id) {
         promotionService.deletePromotion(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(SuccessResponse.empty());
     }
 }
-
